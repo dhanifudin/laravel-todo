319d48b0c0f30be98dd7c5d9f1fc69307154463c
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2025-10-30 19:12:55 +0700

    Integrate authorization bearer into scramble

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use App\Models\Todo;
 use App\Policies\TodoPolicy;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,5 +28,11 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->secure(
+                    SecurityScheme::http(scheme: 'bearer')
+                );
+            });
     }
 }
